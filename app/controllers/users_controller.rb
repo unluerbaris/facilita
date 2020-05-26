9d3666ed9441af1aae04cdea0f797126c74dac75
Grant Holley <gvholley@gmail.com>
2020-05-26 19:11:41 +0900

    Added token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @events = Event.where(user: @user)
+    @events = Event.where(user: @user).reverse
     authorize @user
   end
 
