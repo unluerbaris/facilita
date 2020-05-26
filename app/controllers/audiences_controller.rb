21a32f45fd80cdbcb38a2bb8ac7aa75a9d07e858
Chihiro Tsuda <chihirot0109@gmail.com>
2020-05-26 14:07:51 +0900

    created audience.new function

--- app/controllers/audiences_controller.rb
+++ app/controllers/audiences_controller.rb
@@ -1,2 +1,15 @@
 class AudiencesController < ApplicationController
+
+  def create
+    @event = Event.find_by(token: params[:event_token])
+    @audience = Audience.new
+    @audience.event = @event
+    @audience.user = current_user
+    if @audience.save
+      redirect_to event_path(@event)
+    else
+      flash[:alert] = "Could not join the event"
+      render 'pages/home'
+    end
+  end
 end
