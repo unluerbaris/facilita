c32f206a4e93ba78d2e9589ca0ccab895a4326fa
Baris Unluer <unluerbaris@gmail.com>
2020-05-27 13:22:15 +0900

    Date picking function on event create page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import flatpickr from "flatpickr";
 
 // Internal imports, e.g:
 import { initEventCable } from '../channels/event_channel';
@@ -32,4 +33,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initEventCable();
+  flatpickr(".datepicker", { enableTime: true, defaultDate: new Date() });
 });
