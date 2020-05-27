f751ff858f1918d5dca847425a5e9a09dc605ea8
Grant Holley <gvholley@gmail.com>
2020-05-27 17:04:34 +0900

    .

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -26,6 +26,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:title, :description, :start_time, :end_time)
+    params.require(:event).permit(:title, :description, :start_time, :end_time, :photo)
   end
 end
