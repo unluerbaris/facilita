28847dc6c0529565f3a4513070a5a31fce09192e
Chihiro Tsuda <chihirot0109@gmail.com>
2020-05-28 19:12:21 +0900

    created summaries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
     # devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :photo, :age, :gender, :occupation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :photo, :gender, :occupation, :birthday])
   end
 
   private
