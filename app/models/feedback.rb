99bd2a7d386883c5dd994d373c7515e1ec506e69
Baris Unluer <unluerbaris@gmail.com>
2020-06-02 10:45:55 +0900

    Fixed the bug using only controller

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,7 +2,6 @@ class Feedback < ApplicationRecord
   belongs_to :event
   belongs_to :user
 
-  validates :user, uniqueness: true
   validates :comment, presence: true
   validates :rating, presence: true
   validates :rating, numericality: { greater_than: 0, less_than_or_equal_to: 5,  only_integer: true }
