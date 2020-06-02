e723cb7f3bb7559cf6277fbebdf326c9cc77a1ac
Chihiro Tsuda <chihirot0109@gmail.com>
2020-06-02 21:02:57 +0900

    changed layout

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,7 +1,8 @@
 class Poll < ApplicationRecord
   belongs_to :event
-
   has_many :choices
   has_many :responses, through: :choices
   accepts_nested_attributes_for :choices
+
+  validates :title, presence: :true
 end
