aa304e4bc4d29708bc71d32b6795a2619ca2cdbc
Baris Unluer <unluerbaris@gmail.com>
2020-06-03 11:34:40 +0900

    notification for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,6 +9,7 @@ class QuestionsController < ApplicationController
     @question.event = @event
     @question.user = current_user
     if @question.save
+      Notification.create(recipient: @event.user, actor: current_user, action: "asked", notifiable: @question)
       EventChannel.broadcast_to(
         @event,
         question: render_to_string(partial: "questions/question", locals: { question: @question })
