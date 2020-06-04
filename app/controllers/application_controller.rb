4d9c8fdf26014238e8b1c89b54f79069b4c603bc
Grant Holley <gvholley@gmail.com>
2020-06-04 13:31:50 +0900

    Fixed guest error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
   def current_or_guest_user
     if current_user
       if session[:guest_user_id] && session[:guest_user_id] != current_user.id
-        logging_in
+
         # reload guest_user to prevent caching problems before destruction
         guest_user(with_retry = false).try(:reload).try(:destroy)
         session[:guest_user_id] = nil
