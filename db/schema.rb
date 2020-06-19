f5a26fb4e75a652f88b3bfd50394c2470b236835
Baris Unluer <unluerbaris@gmail.com>
2020-06-19 09:38:24 +0900

    minor changes

--- db/schema.rb
+++ db/schema.rb
@@ -140,6 +140,24 @@ ActiveRecord::Schema.define(version: 2020_06_18_112205) do
     t.index ["user_id"], name: "index_responses_on_user_id"
   end
 
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "target_type", null: false
+    t.bigint "target_id", null: false
+    t.string "key", null: false
+    t.boolean "subscribing", default: true, null: false
+    t.boolean "subscribing_to_email", default: true, null: false
+    t.datetime "subscribed_at"
+    t.datetime "unsubscribed_at"
+    t.datetime "subscribed_to_email_at"
+    t.datetime "unsubscribed_to_email_at"
+    t.text "optional_targets"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["key"], name: "index_subscriptions_on_key"
+    t.index ["target_type", "target_id", "key"], name: "index_subscriptions_on_target_type_and_target_id_and_key", unique: true
+    t.index ["target_type", "target_id"], name: "index_subscriptions_on_target_type_and_target_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -153,8 +171,8 @@ ActiveRecord::Schema.define(version: 2020_06_18_112205) do
     t.string "gender"
     t.string "occupation"
     t.date "birthday"
-    t.integer "age"
     t.boolean "guest", default: false
+    t.integer "age"
     t.string "organization"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
